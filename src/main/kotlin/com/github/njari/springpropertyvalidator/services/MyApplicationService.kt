2664bc84a92997e161f52bf2728c2262d7eccefa
GitHub Action <action@github.com>
2021-09-01 18:03:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/njari/springpropertyvalidator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.njari.springpropertyvalidator.services
+
+import com.github.njari.springpropertyvalidator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
